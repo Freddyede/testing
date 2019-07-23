fd7a090b4ac799f540529ff0a37136ddb4fa9af4
Franck <patouillardfranck3@gmail.com>
2019-07-23 10:04:14 +0200

    Ignore constants front

--- front/src/app/services/messages.service.ts
+++ front/src/app/services/messages.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ROUTESBACK, SERVER } from '../.gitignore/constants/back.const';
+import { ROUTESBACK, SERVER } from '../constants/back.const';
 
 @Injectable({
   providedIn: 'root'
