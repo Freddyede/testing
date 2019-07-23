b86fd08ca61d95dbaf6f5d852559b06c69f52822
Franck <patouillardfranck3@gmail.com>
2019-07-23 11:50:17 +0200

    Editing this.title of Todos and privateToken

--- front/src/app/components/todos/todos.component.ts
+++ front/src/app/components/todos/todos.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.scss']
 })
 export class TodosComponent implements OnInit {
-
+  privateToken: string;
+  title: string;
   constructor() { }
 
   ngOnInit() {
+    this.title = 'Todos';
+    this.privateToken = localStorage.getItem('token');
   }
 
 }
