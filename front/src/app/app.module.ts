01ca7748cd08148ae9d616cad6ad526ec76615fc
Franck <patouillardfranck3@gmail.com>
2019-07-25 17:40:11 +0200

    cd

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {NgxAutoScrollModule} from "ngx-auto-scroll";
-
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
