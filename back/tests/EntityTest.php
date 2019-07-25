05d76c9fe9db6aba410d705c5a115717ea7d592c
Franck <patouillardfranck3@gmail.com>
2019-07-25 23:46:54 +0200

    add comment and entity test

--- back/tests/EntityTest.php
+++ back/tests/EntityTest.php
@@ -2,11 +2,13 @@
 
 namespace App\Tests;
 
+use App\Entity\Pages;
 use PHPUnit\Framework\TestCase;
 
 class EntityTest extends TestCase
 {
     public function testPagesEntity() {
-        $this->assertTrue(true);
+        $page = new Pages();
+        
     }
 }
